1dc5cf7e3ef711cf84d811c156b4a5fa8b8eefe8
Auston Stewart <auston@shedbuilt.net>
2018-01-10 18:14:08 -0500

    Updating to 2017c

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-ZONEINFO=${SHED_FAKEROOT}/usr/share/zoneinfo
+ZONEINFO="${SHED_FAKEROOT}/usr/share/zoneinfo"
 mkdir -pv $ZONEINFO/{posix,right}
 for tz in etcetera southamerica northamerica europe africa antarctica  \
           asia australasia backward pacificnew systemv; do
-    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz}
-    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz}
-    zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz}
+    zic -L /dev/null   -d $ZONEINFO       -y "sh yearistype.sh" ${tz} || return 1
+    zic -L /dev/null   -d $ZONEINFO/posix -y "sh yearistype.sh" ${tz} || return 1
+    zic -L leapseconds -d $ZONEINFO/right -y "sh yearistype.sh" ${tz} || return 1
 done
-cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
-zic -d $ZONEINFO -p America/New_York
+cp -v zone.tab zone1970.tab iso3166.tab "$ZONEINFO"
+zic -d "$ZONEINFO" -p America/New_York || return 1
 unset ZONEINFO
